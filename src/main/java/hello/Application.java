d5bffd354332edcf14d910c29c992c6af2ebc85b
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-02-28 16:25:26 -0500

    adding in REST service

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by johnsquier on 2/21/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
