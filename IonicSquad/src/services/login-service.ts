974925bcac0eefcbb2298f17ff46a0e13ea8456c
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-03-01 09:26:06 -0500

    cleaned up style and var types in IonicSquad dir

--- IonicSquad/src/services/login-service.ts
+++ IonicSquad/src/services/login-service.ts
@@ -3,19 +3,24 @@ import { Http } from '@angular/http';
 
 @Injectable()
 export class LoginService{
-    public data:any;
-    public loginStatus: any = "FALSE";
-    constructor(private  http:Http){}
-    str1:string = "http://127.0.0.1:8080/login?name=";
-    
 
-    getLoginStatus(username:any, password:any){
+    public data: any;
+    private loginStatus: string = "false";
+    private str1: string = "http://127.0.0.1:8080/login?name=";
+
+    constructor(private http: Http){
+
+    }
+
+    getLoginStatus(username: string, password: string){
+        // log the query
         console.log(this.str1+username+"&pw="+password);
+
         this.http.get(this.str1+username+"&pw="+password).subscribe(res => {
             this.data = res.json();
             this.loginStatus = this.data.isPasswordValid;
-            console.log("data:   "+this.data);
-            console.log("status   "+this.loginStatus);
+            // log response
+            console.log(this.data);
         }, error => {
             console.log(error);
         });
