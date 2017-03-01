b79003cb0e259916b8732a9ebe848cad1dc843b2
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-03-01 09:32:45 -0500

    small changes

--- restful/src/test/java/team/squad/ControllerTests.java
+++ restful/src/test/java/team/squad/ControllerTests.java
@@ -5,20 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.mock.web.MockHttpSession;
+
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Team Squad Spring Squad
@@ -30,7 +24,6 @@ public class ControllerTests {
 
     @Autowired
     private MockMvc mockMvc;
-    private MockHttpSession mockHttp;
 
     @Test
     public void goodRequestShouldReturnTrue() throws Exception {
