4d62230a0b241caf372cae841ce7cb3e0a26e201
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-03-01 08:15:21 -0500

    started new spring project in restful/

---
+++ restful/src/main/java/team/squad/UservalidationApplication.java
@@ -0,0 +1,12 @@
+package team.squad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UservalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UservalidationApplication.class, args);
+	}
+}
